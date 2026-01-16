39b4cadb52589c07341e849d57234fbb27ad0b88
Snt <sant85082@gmail.com>
2026-01-15 21:37:51 -0500

    Early correction with the atribute access of the class 'ClienteProducto', set private

--- Includes/usuario/cliente_productos.h
+++ Includes/usuario/cliente_productos.h
@@ -6,8 +6,8 @@
 namespace Maquina_Expendedora {
     class ClienteProducto {
         //Class for add products that the Costumer wants
-    public:
         std::vector<std::string>productos_deseados;
+    public:
         void AnadirProducto(std::string obj_producto_deseado); //Set the vector with the products
 
         const std::vector<std::string> RetornarProductos() const {
